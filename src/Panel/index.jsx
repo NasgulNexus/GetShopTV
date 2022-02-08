30719f1b3ef847561692badb375308f5cf829ccf
NasgulNexus <moctegor@gmail.com>
2022-02-08 20:52:36 +0300

    отображение цифра при нажатие на кнопки

--- src/Panel/index.jsx
+++ src/Panel/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import ButtonNumber from "../ButtonNumber";
 
@@ -10,30 +10,54 @@ const buttonValue = [
 ];
 
 const Panel = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handlerNumber = e => {
+    const value = e.target.innerHTML;
+    if (phoneNumber.length < 10) {
+      setPhoneNumber(phoneNumber + value);
+    }
+  };
+
+  const handlerDelete = () => {
+    setPhoneNumber(function() {
+      return phoneNumber.slice(0, -1);
+    });
+  };
+
+  const handleChange = e => {
+    const value = e.target.value;
+    if (value.length < 11) {
+      setPhoneNumber(value);
+    }
+  };
+
   return (
     <div className="PanelContainer">
       <p className="PanelMainText">
         Введите ваш номер мобильного&nbsp;телефона
       </p>
-      <input className="PanelInput" />
+      <input
+        value={phoneNumber}
+        className="PanelInput"
+        onChange={handleChange}
+      />
       <p className="PanelInputText">
         и с Вами свяжется наш менеждер для дальнейшей&nbsp;консультации
       </p>
       <div className="PanelDiv">
         <div className="PanelButtonBox">
-          {buttonValue.flat().map((btn, i) => {
+          {buttonValue.flat().map(btn => {
             return (
               <ButtonNumber
-                key={i}
+                key={btn}
                 className={
                   btn === "стереть"
                     ? "ButtonNumber ButtonDelete"
                     : "ButtonNumber"
                 }
                 value={btn}
-                onClick={() => {
-                  console.log(`${btn} clicked!`);
-                }}
+                onClick={btn === "стереть" ? handlerDelete : handlerNumber}
               />
             );
           })}
@@ -45,7 +69,9 @@ const Panel = () => {
           Согласие на обработку персональных&nbsp;данных
         </label>
       </div>
-      <button disabled={true} className="PanelButton">Подтвердить номер</button>
+      <button disabled={true} className="PanelButton">
+        Подтвердить номер
+      </button>
     </div>
   );
 };
