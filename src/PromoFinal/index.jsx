d749b65ae5590cc5c6b4c36d35f771628e8723e7
NasgulNexus <moctegor@gmail.com>
2022-02-08 22:20:42 +0300

    создал компонент promoFinal

---
+++ src/PromoFinal/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./index.css";
+import Slider from "../Slider";
+
+const PromoFinal = () => {
+  return <Slider />;
+};
+
+export default PromoFinal;
--- src/RouterHead/index.jsx
+++ src/RouterHead/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import PromoMain from "../PromoMain";
 import Promo from "../Promo";
+import PromoFinal from "../PromoFinal";
 
 const RouterHead = () => (
   <div>
     <Routes>
       <Route path="/" element={<PromoMain />}></Route>
       <Route path="/promo" element={<Promo />}></Route>
+      <Route path="/promofinal" element={<PromoFinal />}></Route>
     </Routes>
   </div>
 );
