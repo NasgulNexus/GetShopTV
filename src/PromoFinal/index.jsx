7c52102625563ce92bf99272173602ea0caa847f
NasgulNexus <moctegor@gmail.com>
2022-02-08 22:26:04 +0300

    добавил кнопку закрытия формы к промо-финал

--- src/PromoFinal/index.jsx
+++ src/PromoFinal/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./index.css";
 import Slider from "../Slider";
+import ButtonClose from "../ButtonClose";
+import "../Promo/index.css";
 
 const PromoFinal = () => {
-  return <Slider />;
+  return (
+    <div>
+      <Slider />
+      <div className="PromoButtonClose">
+        <ButtonClose />
+      </div>
+    </div>
+  );
 };
 
 export default PromoFinal;
