fa3722a00c6bdf7cd66e8c62b0a658e12baa5b63
NasgulNexus <moctegor@gmail.com>
2022-02-10 16:22:47 +0300

    получаю данные при нажатие с клавиатуры

--- src/Promo/index.jsx
+++ src/Promo/index.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 import ButtonClose from "../ButtonClose";
 import Panel from "../Panel";
 import QRPromoInformation from "../QRPromoInformation";
 
 const Promo = () => {
+  const [key, setKey] = useState("");
+
+  useEffect(() => {
+    const keyUp = event => {
+      console.log(
+        "Клавиша " + event.key + " отпущена. Её код - " + event.keyCode
+      );
+      setKey(event.key);
+    };
+    document.addEventListener("keyup", keyUp);
+
+    return () => {
+      document.removeEventListener("keyup", keyUp);
+    };
+  }, []);
+
   return (
     <div>
       <div className="PromoButtonClose">
