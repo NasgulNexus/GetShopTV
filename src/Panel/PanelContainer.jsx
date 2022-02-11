9d8376e56f7703af4b8823e2d4b4b963c75f9e30
NasgulNexus <moctegor@gmail.com>
2022-02-11 22:53:01 +0300

    Updates

--- src/Panel/PanelContainer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./index.css";
-
-const PanelContainer = ({ children }) => {
-  return <div className="PanelContainer">{children}</div>;
-};
-
-export default PanelContainer;
